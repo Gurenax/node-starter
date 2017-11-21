70d8e092f0b05f80ab3a90bb5cd9f376dfe29152
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-22 10:08:20 +1100

    Add example three

--- src/2-numberplate.js
+++ src/2-numberplate.js
@@ -21,10 +21,11 @@ Challenges:
 */
 
 const sentence = 'JavaScript (/ˈdʒɑːvəˌskrɪpt/[6]), often abbreviated as JS, is a high-level, dynamic, weakly typed, prototype-based, multi-paradigm, and interpreted programming language. Alongside HTML and CSS, JavaScript is one of the three core technologies of World Wide Web content production. It is used to make webpages interactive and provide online programs, including video games. The majority of websites employ it, and all modern web browsers support it without the need for plug-ins by means of a built-in JavaScript engine. Each of the many JavaScript engines represent a different implementation of JavaScript, all based on the ECMAScript specification, with some engines not supporting the spec fully, and with many engines supporting additional features beyond ECMA.';
+const three = 'To infinity and beyond'
 
 // Replace a string with a wordBefore to a wordAfter
 const replaceString = (str, wordBefore, wordAfter ) => {
-  return str.replace(new RegExp(wordBefore,'gi'),wordAfter)
+  return str.replace(new RegExp(wordBefore, 'gi'), wordAfter)
 }
 
 // Restrict a string to n characters
@@ -35,8 +36,8 @@ const firstNCharacters = (str, n) => {
 console.log( replaceString(two, 'for', '4') )
 console.log( replaceString(formatVanityPlate(two), 'for', '4') )
 console.log( '----- ')
-console.log( replaceString(two, 'to', '2') )
-console.log( replaceString(formatVanityPlate(two), 'to', '2') )
+console.log( replaceString(three, 'to', '2') )
+console.log( replaceString(formatVanityPlate(three), 'to', '2') )
 console.log( '----- ')
 console.log( replaceString(sentence, 'for', '4') )
 console.log( '----- ')
